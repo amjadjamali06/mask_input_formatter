50c7ac5fb696e56e59bb89d35701f8e7f386b8c4
Amjad Ali <amjad.ali@sapphire.co>
2021-06-07 13:13:21 +0500

    Committed By: Amjad Jamali. Updated README, LICENSE and CHANGELOG files.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,7 +8,6 @@ import 'package:mask_input_formatter/mask_input_formatter.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -16,9 +15,8 @@ class MyApp extends StatelessWidget {
       home: UsersData(),
     );
   }
-
 }
-
+/// An Example class to test [MaskInputFormatter]
 class UsersData extends StatefulWidget {
   const UsersData({Key key}) : super(key: key);
 
@@ -27,68 +25,67 @@ class UsersData extends StatefulWidget {
 }
 
 class _UsersData extends State<UsersData> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         backgroundColor: Colors.grey.shade200,
         body: SafeArea(
             child: Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      inputFormatters: [
-                        // Binding MaskInputFormatter with TextField
-                        MaskInputFormatter(mask: '#####-#######-#'),
-                      ],
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "42000-0000000-0",
-                      ),
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.number,
-                    ),
-                    SizedBox(height: 4),
-                    TextFormField(
-                      // another example with text and numbers
-                      inputFormatters: [MaskInputFormatter(mask: '(AA) ### #######', textAllCaps: true)],
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "(AB) 301 4567890",
-                      ),
-                      textInputAction: TextInputAction.next,
-                    ),
-                    SizedBox(height: 4),
-                    TextFormField(
-                      inputFormatters: [
-                        MaskInputFormatter(mask: '+# (###) ###-##-##'),
-                      ],
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "+1 (234) 567-89-01",
-                      ),
-                      textInputAction: TextInputAction.next,
-                    ),
-                    SizedBox(height: 4),
-                    TextFormField(
-                      inputFormatters: [
-                        MaskInputFormatter(mask: '##/##/####'),
-                      ],
-                      textInputAction: TextInputAction.next,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "23/05/2021",
-                      ),
-                    ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  inputFormatters: [
+                    // Binding MaskInputFormatter with TextField
+                    MaskInputFormatter(mask: '#####-#######-#'),
                   ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "42000-0000000-0",
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
                 ),
-              ),
-            )
-        )
-    );
+                SizedBox(height: 4),
+                TextFormField(
+                  // another example with text and numbers
+                  inputFormatters: [
+                    MaskInputFormatter(
+                        mask: '(AA) ### #######', textAllCaps: true)
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "(AB) 301 4567890",
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(height: 4),
+                TextFormField(
+                  inputFormatters: [
+                    MaskInputFormatter(mask: '+# (###) ###-##-##'),
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "+1 (234) 567-89-01",
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(height: 4),
+                TextFormField(
+                  inputFormatters: [
+                    MaskInputFormatter(mask: '##/##/####'),
+                  ],
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "23/05/2021",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )));
   }
 }
--- lib/mask_input_formatter.dart
+++ lib/mask_input_formatter.dart
@@ -5,66 +5,79 @@ import 'package:flutter/services.dart';
 
 /* @Autor Amjad Jamali */
 
+/// A [MaskInputFormatter] class is used for masking text inserted in [TextField] and [TextFormField].
+///
+/// This is a class which extends [TextInputFormatter] class,
+/// [TextInputFormatter] can be optionally injected into an [EditableText]
+/// to provide as-you-type validation and formatting of the text being edited.
+///
 class MaskInputFormatter extends TextInputFormatter {
-
   List<String> _maskChars = [];
   List<int> _maskIndices = [];
 
   String _letters = 'abcdefghijklmnopqrstuvwxyz';
   String _numbers = '1234567890';
-  String _regex='';
+  String _regex = '';
   String _mask;
-  String _allowableValues='';
-  String _newChar='';
+  String _allowableValues = '';
+  String _newChar = '';
 
   bool _textAllCaps = false;
-  bool _isBackPressed=false;
+  bool _isBackPressed = false;
 
-  int _maxLength=0, _newLength=0, _oldLength=0;
+  int _maxLength = 0, _newLength = 0, _oldLength = 0;
 
   int _offset;
 
-  MaskInputFormatter({@required String mask, bool textAllCaps} ){
-    _textAllCaps = textAllCaps!=null;
+  /// The constructor of [MaskInputFormatter] class accepts two arguments.
+  /// 1. [mask] this is required parameter, text will be formatted on the basis of this mask,
+  /// 2. [textAllCaps] this is optional, when user wants all letters capital then he can set [textAllCaps] = true,
+  MaskInputFormatter({@required String mask, bool textAllCaps}) {
+    _textAllCaps = textAllCaps != null;
     this._mask = mask;
+
     /// checking regex in mask
-    if(mask.contains('#') && mask.contains('A') ) _regex = _letters+_numbers;
-    else if(mask.contains('#')) _regex = _numbers;
-    else if(mask.contains('A')) _regex = _letters;
+    if (mask.contains('#') && mask.contains('A'))
+      _regex = _letters + _numbers;
+    else if (mask.contains('#'))
+      _regex = _numbers;
+    else if (mask.contains('A'))
+      _regex = _letters;
     else {
       print('invalid mask \'$mask\' (Ex.\'AAA-###\')');
-      _mask='';
+      _mask = '';
       return;
     }
     _maxLength = mask.length;
 
     int maskPos = 0;
+
     /// separating mask characters and positions form applied mask
-    for (int i =0 ;i<_maxLength;) {
+    for (int i = 0; i < _maxLength;) {
       String ch = mask[i];
-      if(ch!='#' && ch!='A'){
+      if (ch != '#' && ch != 'A') {
         String sChar = '';
-        for(;(sChar!='#' && sChar!='A' && i<_maxLength);){
-          ch=ch+sChar;
+        for (; (sChar != '#' && sChar != 'A' && i < _maxLength);) {
+          ch = ch + sChar;
           i++;
-          if(i<_maxLength) {
+          if (i < _maxLength) {
             sChar = mask[i];
           }
         }
         _maskIndices.add(maskPos);
         _maskChars.add(ch);
         maskPos++;
-      }else{
+      } else {
         i++;
         maskPos++;
-        _allowableValues=_allowableValues+ch;
+        _allowableValues = _allowableValues + ch;
       }
     }
   }
 
   /// method to format user input into given mask
-  TextEditingValue _formatText(TextEditingValue oldValue, TextEditingValue newValue) {
-
+  TextEditingValue _formatText(
+      TextEditingValue oldValue, TextEditingValue newValue) {
     String newText = _getUnmaskedText(newValue.text);
 
     newText = _getValidText(newText);
@@ -79,113 +92,121 @@ class MaskInputFormatter extends TextInputFormatter {
       _offset--;
     }
 
-    int maskSpaces=0;
+    int maskSpaces = 0;
+
     /// maintain offset(cursor position) occording to given mask
     if (!_isBackPressed) {
-      for(int mIndex, i=0 ;i<_maskIndices.length; i++){
-        mIndex = _maskIndices.elementAt(i)+maskSpaces+1;
-        _offset = (_offset == mIndex) ? _offset+_maskChars.elementAt(i).length : _offset;
-        maskSpaces = (maskSpaces+_maskChars.elementAt(i).length-1);
+      for (int mIndex, i = 0; i < _maskIndices.length; i++) {
+        mIndex = _maskIndices.elementAt(i) + maskSpaces + 1;
+        _offset = (_offset == mIndex)
+            ? _offset + _maskChars.elementAt(i).length
+            : _offset;
+        maskSpaces = (maskSpaces + _maskChars.elementAt(i).length - 1);
       }
 
-      for(int i=0 ; i < _maskIndices.length ; i++) {
-        if(_maskIndices[i] == (_offset-i+(i>0?1:0))) {
-          _offset = _maskChars[i].length+_offset;
+      for (int i = 0; i < _maskIndices.length; i++) {
+        if (_maskIndices[i] == (_offset - i + (i > 0 ? 1 : 0))) {
+          _offset = _maskChars[i].length + _offset;
           break;
         }
       }
     } else {
-      while(newText == oldText && _offset>1 && !_regex.contains(filteredText[_offset-1].toLowerCase())) {
+      while (newText == oldText &&
+          _offset > 1 &&
+          !_regex.contains(filteredText[_offset - 1].toLowerCase())) {
         _offset--;
       }
     }
 
-    while(_offset<filteredText.length && !_isBackPressed && !_regex.contains(filteredText[_offset])){
+    while (_offset < filteredText.length &&
+        !_isBackPressed &&
+        !_regex.contains(filteredText[_offset])) {
       _offset++;
     }
 
-    if(_offset>filteredText.length){
+    if (_offset > filteredText.length) {
       _offset = filteredText.length;
     }
 
     /// returning new value with masked text
     return TextEditingValue(
-      text: _textAllCaps?filteredText.toUpperCase():filteredText,
+      text: _textAllCaps ? filteredText.toUpperCase() : filteredText,
       selection: TextSelection(
           baseOffset: _offset,
           extentOffset: _offset,
           affinity: newValue.selection.affinity,
-          isDirectional: newValue.selection.isDirectional
-      ),
+          isDirectional: newValue.selection.isDirectional),
     );
   }
 
-
+  /// method to format user input into given mask
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    if(_mask==''){
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (_mask == '') {
       return newValue;
     }
     _newLength = newValue.text.length;
     _oldLength = oldValue.text.length;
-    _isBackPressed = _newLength<_oldLength;
+    _isBackPressed = _newLength < _oldLength;
 
-    if(_isBackPressed){
+    if (_isBackPressed) {
       return _formatText(oldValue, newValue);
-    }else {
-      /// get new inserted character 
+    } else {
+      /// get new inserted character
       _newChar = newValue.text[oldValue.selection.baseOffset];
     }
 
-    if(!_regex.contains(_newChar) && _oldLength == _newLength-1 ){
+    if (!_regex.contains(_newChar) && _oldLength == _newLength - 1) {
       /// if new value not contain allowable values return old value
       return oldValue;
-    }else if(_oldLength==_maxLength && _newLength>=_maxLength){
+    } else if (_oldLength == _maxLength && _newLength >= _maxLength) {
       /// if already inserted max chars return old value
       return oldValue;
-    }else if(newValue.composing.start!=-1
-        && !_letters.contains(newValue.text[oldValue.selection.baseOffset])
-        && _newLength>_oldLength
-    ){
-      if(_newLength>_oldLength){
-        return  _formatText(oldValue, TextEditingValue(
-          text: newValue.text.substring(0, _maxLength),
-          selection: newValue.selection,
-        ));
+    } else if (newValue.composing.start != -1 &&
+        !_letters.contains(newValue.text[oldValue.selection.baseOffset]) &&
+        _newLength > _oldLength) {
+      if (_newLength > _oldLength) {
+        return _formatText(
+            oldValue,
+            TextEditingValue(
+              text: newValue.text.substring(0, _maxLength),
+              selection: newValue.selection,
+            ));
       }
       return oldValue;
-    }else{
+    } else {
       /// format input value
       return _formatText(oldValue, newValue);
     }
   }
 
-
   /// check if number
-  bool _isNumber(String ch){
+  bool _isNumber(String ch) {
     return _numbers.contains(ch);
   }
 
   /// check if Letter
-  bool _isLetter(String ch){
+  bool _isLetter(String ch) {
     return _letters.contains(ch.toLowerCase());
   }
 
-  bool _isValid(String ch){
+  bool _isValid(String ch) {
     return _regex.contains(ch.toLowerCase());
   }
 
-  /// validate text 
-  String _getValidText(String temp){
-    String text='';
-    for (int i = 0, j=0; i < temp.length && j<_allowableValues.length; i++) {
+  /// validate text
+  String _getValidText(String temp) {
+    String text = '';
+    for (int i = 0, j = 0;
+        i < temp.length && j < _allowableValues.length;
+        i++) {
       String ch = temp[i];
       String regCh = _allowableValues[j];
-      if(regCh == 'A' && _isLetter(ch)) {
+      if (regCh == 'A' && _isLetter(ch)) {
         text = text + ch;
         j++;
-      }
-      else if(regCh == '#' && _isNumber(ch)) {
+      } else if (regCh == '#' && _isNumber(ch)) {
         text = text + ch;
         j++;
       }
@@ -194,28 +215,30 @@ class MaskInputFormatter extends TextInputFormatter {
   }
 
   /// get unmasked text
-  String _getUnmaskedText(String temp){
+  String _getUnmaskedText(String temp) {
     String text = '';
     for (int i = 0; i < temp.length; i++) {
       String ch = temp[i];
-      text = text + (_isValid(ch)?ch:'');
+      text = text + (_isValid(ch) ? ch : '');
     }
     return text;
   }
 
   /// get masked text
-  String _getMaskedText(String newText, String oldText){
-    String text='';
-    for (int i = 0, j = 0; i < newText.length; ) {
-      if (j < _maskChars.length && i == _maskIndices[j]-j) {
+  String _getMaskedText(String newText, String oldText) {
+    String text = '';
+    for (int i = 0, j = 0; i < newText.length;) {
+      if (j < _maskChars.length && i == _maskIndices[j] - j) {
         text = text + _maskChars[j];
         j++;
       }
       text = text + newText[i];
       i++;
 
-      if(i == newText.length && newText.length >= oldText.length){
-        if (((_offset>=text.length)||newText.length==1) && j < _maskChars.length && i == _maskIndices[j]-j) {
+      if (i == newText.length && newText.length >= oldText.length) {
+        if (((_offset >= text.length) || newText.length == 1) &&
+            j < _maskChars.length &&
+            i == _maskIndices[j] - j) {
           text = text + _maskChars[j];
           j++;
         }
@@ -223,5 +246,4 @@ class MaskInputFormatter extends TextInputFormatter {
     }
     return text;
   }
-
 }
